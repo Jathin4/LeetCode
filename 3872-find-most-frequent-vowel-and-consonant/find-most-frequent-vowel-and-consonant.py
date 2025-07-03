efd1f3ffc28fd883a96e225c805262edaf405481
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2025-07-03 08:35:26 +0530

    Time: 7 ms (52.68%) | Memory: 12.5 MB (13.29%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxFreqSum(self, s):
+        vowel = {'a','e','i','o','u'}
+        vow = [0] * 26
+        const = [0] * 26
+        
+        for ch in s:
+            idx = ord(ch) - ord('a')
+            if ch in vowel:
+                vow[idx] += 1
+            else:
+                const[idx] += 1
+        
+        return max(vow) + max(const)
+        
