99bc395cf1dad1f285121c5760a524036988cfa4
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2025-07-28 09:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (50.43%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        idx = word.find(ch)
+        if idx == -1:
+            return word
+        return word[:idx+1][::-1] + word[idx+1:]
+
+                
+
+
+        
