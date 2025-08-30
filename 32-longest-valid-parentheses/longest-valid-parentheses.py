ee2502b73ebdaf0fbfda8680150128236b8af2e1
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2025-08-30 10:02:25 +0530

    Time: 26 ms (6.81%) | Memory: 13.9 MB (7.11%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack=[]
+        l=['0']*len(s)
+        for ind,i in enumerate(s):
+            if i=='(':
+                stack.append(ind)
+            else:
+                if stack:
+                    l[stack.pop()]='1'
+                    l[ind]='1'
+        return max(len(i) for i in ''.join(l).split('0'))
+        
