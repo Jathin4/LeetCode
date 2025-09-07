eea80a40678f6c7e33582cbddb921133f224cab1
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2025-09-07 17:06:08 +0530

    Time: 126 ms (26.75%) | Memory: 44.4 MB (74.22%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        v1, v2, ans = [], [], []
+        
+        for num in nums:
+            if num > 0:
+                v1.append(num)
+            else:
+                v2.append(num)
+        
+        ind1, ind2 = 0, 0
+        
+        while ind2 < len(nums) // 2:
+            ans.append(v1[ind1])
+            ind1 += 1
+            ans.append(v2[ind2])
+            ind2 += 1
+        
+        return ans 
+        
