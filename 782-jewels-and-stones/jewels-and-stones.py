215298909d06d588ed7c16e71edf54ac16649fd3
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2026-06-17 08:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.72%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        j = set(jewels)
+        return sum(x in j for x in stones)
+        
